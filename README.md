c78dcb866de9f175a47c43d13e1baaab0b644993
Paolo van Reyk <paolo.vanreyk@gmail.com>
2023-07-17 14:08:14 +1000

    ReadME update, Really not sure how to fix localstorage

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Weather-Dashboard
 An app to view the five day of cities from all over the world
+
+## Usage
+Users can input a city of their choice from all over the world in the aside seach bar, their most recent search will be recorded. A large display of the current weather, along with the current date will appear in the main section. Underneath that a display of weather of the tnext five days will appear below in smaller boxes
+
+## Deployed Link
+https://anuf0.github.io/Weather-Dashboard/\
+
+## Screenshot
+![screenshot](./assets/screenshot.png)
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const apiKey = '055b3d12f20ab467916355aefa4d41a3';
 const cityListEl = $('#city-list');
-const cityName = JSON.parse(localStorage.getItem('storedCities'))[0];
+const cityName = JSON.parse(localStorage.getItem('storedCities'));
 const storedCities = [];
 
 //API URLS
@@ -62,40 +62,40 @@ $.ajax ({
         const dayOne = dayjs(response.list[0].dt_txt).format("ddd, MMM D");
 
         // Adds day 1 data to page
-        $(".day-one-temperature").text("Temp: " + response.list[0].main.temp + " C");
-        $(".day-one-date").html("<h6>" + dayOne + "</h6>");
-        $(".day-one-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-        $(".day-one-humidity").text("Humidity: " + response.list[0].main.humidity + "%");
+        $("#day-one-temperature").text("Temp: " + response.list[0].main.temp + " C");
+        $("#day-one-date").html("<h6>" + dayOne + "</h6>");
+        $("#day-one-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+        $("#day-one-humidity").text("Humidity: " + response.list[0].main.humidity + "%");
 
         const dayTwo = dayjs(response.list[8].dt_txt).format("ddd, MMM D");
         // Adds day 2 data to page
-        $(".day-two-temperature").text("Temp: " + response.list[8].main.temp + " C");
-        $(".day-two-date").html("<h6>" + dayTwo + "</h6>");
-        $(".day-two-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-        $(".day-two-humidity").text("Humidity: " + response.list[8].main.humidity + "%");
+        $("#day-two-temperature").text("Temp: " + response.list[8].main.temp + " C");
+        $("#day-two-date").html("<h6>" + dayTwo + "</h6>");
+        $("#day-two-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[8].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+        $("#day-two-humidity").text("Humidity: " + response.list[8].main.humidity + "%");
 
         const dayThree = dayjs(response.list[16].dt_txt).format("ddd, MMM D");
         // Adds day 3 data to page
-        $(".day-three-temperature").text("Temp: " + response.list[16].main.temp + " C");
-        $(".day-three-date").html("<h6>" + dayThree + "</h6>");
-        $(".day-three-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-        $(".day-three-humidity").text("Humidity: " + response.list[16].main.humidity + "%");
+        $("#day-three-temperature").text("Temp: " + response.list[16].main.temp + " C");
+        $("#day-three-date").html("<h6>" + dayThree + "</h6>");
+        $("#day-three-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[16].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+        $("#day-three-humidity").text("Humidity: " + response.list[16].main.humidity + "%");
 
         const dayFour = dayjs(response.list[24].dt_txt).format("ddd, MMM D");
 
         // Adds day 4 data to page
-        $(".day-four-temperature").text("Temp: " + response.list[24].main.temp + " C");
-        $(".day-four-date").html("<h6>" + dayFour + "</h6>");
-        $(".day-four-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-        $(".day-four-humidity").text("Humidity: " + response.list[24].main.humidity + "%");
+        $("#day-four-temperature").text("Temp: " + response.list[24].main.temp + " C");
+        $("#day-four-date").html("<h6>" + dayFour + "</h6>");
+        $("#day-four-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[24].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+        $("#day-four-humidity").text("Humidity: " + response.list[24].main.humidity + "%");
 
         const dayFive = dayjs(response.list[32].dt_txt).format("ddd, MMM D");
 
         // Adds day 5 data to page
-        $(".day-five-temperature").text("Temp: " + response.list[32].main.temp + " C");
-        $(".day-five-date").html("<h6>" + dayFive + "</h6>");
-        $(".day-five-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-        $(".day-five-humidity").text("Humidity: " + response.list[32].main.humidity + "%");
+        $("#day-five-temperature").text("Temp: " + response.list[32].main.temp + " C");
+        $("#day-five-date").html("<h6>" + dayFive + "</h6>");
+        $("#day-five-icon").html("<img src='https://openweathermap.org/img/w/" + response.list[32].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+        $("#day-five-humidity").text("Humidity: " + response.list[32].main.humidity + "%");
 
     });
 
